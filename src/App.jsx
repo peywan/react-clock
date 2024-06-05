46f95ce43479d3e10184794b917d6a84d0120014
Peywan Sabir <peywan1999@icloud.com>
2024-06-05 15:47:21 +0200

    exercise for now only

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import React, { useState } from 'react'
+
 
 function App() {
+    const [count, setCount] = useState(0)
 
+    const increment = () => {
+        setCount(count + 1)
+    }
+    const decrement = () => {
+        setCount(count - 1)
+    }
   return (
     <>
         <h1>react clock by Peywan!</h1>
+        <p>the count is: {count}</p>
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
     </>
   )
 }
