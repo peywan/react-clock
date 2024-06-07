d01f788737df374bc6d9d90ab33b614489542764
Peywan Sabir <peywan1999@icloud.com>
2024-06-07 13:43:40 +0200

    boilerplate clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import './App.css'
-import React, { useState } from 'react'
+// Importera React och hooks
+import { useState, useEffect } from "react";
+const App = () => {
+    return (
+        <>
+            <h1>your locale time zone time by peywan with react</h1>
+            // Skapa en state variabel för tiden
+
+            // Använd useEffect för att sätta upp setInterval
+            // returnera JSX som visar tiden
 
 
-function App() {
-    const [count, setCount] = useState(0)
-
-    const increment = () => {
-        setCount(count + 1)
-    }
-    const decrement = () => {
-        setCount(count - 1)
-    }
-  return (
-    <>
-        <h1>react clock by Peywan!</h1>
-        <p>the count is: {count}</p>
-        <button onClick={decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={increment}>+</button>
-    </>
-  )
+        </>
+    )
 }
-
 export default App
