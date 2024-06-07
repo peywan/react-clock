5c9c8b01214d2cd177d2d24de7512c35aeb6d9ec
Peywan Sabir <peywan1999@icloud.com>
2024-06-07 14:02:19 +0200

    clock repository complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 // Importera React och hooks
 import { useState, useEffect } from "react";
+
+
 const App = () => {
+    // Skapa en state variabel för tiden
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    // Använd useEffect för att sätta upp setInterval
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(interval)
+    }, []);
+
     return (
         <>
+            {/*returnera JSX som visar tiden*/}
             <h1>your locale time zone time by peywan with react</h1>
-            // Skapa en state variabel för tiden
-
-            // Använd useEffect för att sätta upp setInterval
-            // returnera JSX som visar tiden
-
-
+            <h5>the current time is: {time}</h5>
         </>
     )
 }
